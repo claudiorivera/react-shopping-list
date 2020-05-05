f556d13c5985ad5520fce0db1507fd9787bb4988
Claudio Rivera <me@claudiorivera.com>
2020-05-05 16:22:48 -0500

    Server and DB config done

--- server.js
+++ server.js
@@ -2,12 +2,29 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 
+// Environmental values
+const PORT = process.env.PORT;
+const MONGO_URI = process.env.MONGO_URI;
+
+// Instantiate Express
 const app = express();
 
 // JSON middleware parser
 app.use(express.json());
 
-// DB config
-const db = process.env.MONGO_URI;
+// DB config - per https://mongoosejs.com/docs/deprecations.html
+mongoose.set("useNewUrlParser", true);
+mongoose.set("useFindAndModify", false);
+mongoose.set("useCreateIndex", true);
+mongoose.set("useUnifiedTopology", true);
 
-console.log(db);
+// DB connect
+mongoose
+  .connect(MONGO_URI)
+  .then(() => console.log("DB connected..."))
+  .catch((error) => console.log(error));
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
