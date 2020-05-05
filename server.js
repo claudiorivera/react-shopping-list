2a937dd5ea57b169986f196487f3642dc6e80e63
Claudio Rivera <me@claudiorivera.com>
2020-05-05 15:45:46 -0500

    MongoDB configured

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 
@@ -5,3 +6,8 @@ const app = express();
 
 // JSON middleware parser
 app.use(express.json());
+
+// DB config
+const db = process.env.MONGO_URI;
+
+console.log(db);
