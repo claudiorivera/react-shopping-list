13570a08bd604091b8cded260a8f586259b448fe
Claudio Rivera <me@claudiorivera.com>
2020-05-27 11:45:22 -0500

    Fixed path call

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use("/items", items);
 
 // Serve client/build folder in production
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path(__dirname, "client", "build")));
+  app.use(express.static(path.join(__dirname, "client", "build")));
 }
 
 // Start server
