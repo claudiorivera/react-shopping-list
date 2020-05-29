0367ef37819ea8bb8ffefccbc64c0c0dd9766c1f
Claudio Rivera <me@claudiorivera.com>
2020-05-29 09:39:58 -0500

    Implemented react-devtools during development

--- client/src/index.js
+++ client/src/index.js
@@ -1,3 +1,4 @@
+import "react-devtools"; // Standalone react-devtools for Safari - Remove for production
 import React from "react";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
